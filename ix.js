b967e94c4ce9166ccc2b2ce0ab069e47c2b1caf9
Shoreditch Design <developer@shoreditchdesignstudio.com>
2025-07-03 12:02:51 +0100

    changes

--- ix.js
+++ ix.js
@@ -8,3 +8,27 @@ document
       currentState === "open" ? "closed" : "open",
     );
   });
+
+window.addEventListener(
+  "touchmove",
+  function (event) {
+    // Prevent multi-touch gestures
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  },
+  { passive: false },
+);
+
+window.addEventListener("scroll", function () {
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+  const scrollHeight = document.documentElement.scrollHeight;
+  const clientHeight = document.documentElement.clientHeight;
+
+  if (scrollTop < 0) {
+    window.scrollTo(0, 0);
+  }
+  if (scrollTop + clientHeight >= scrollHeight) {
+    window.scrollTo(0, scrollHeight - clientHeight);
+  }
+});
